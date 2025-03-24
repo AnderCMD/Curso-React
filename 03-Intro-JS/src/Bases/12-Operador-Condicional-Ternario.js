2d29f6eb0e3aa73fc039c186efe4b8dc029c4dc2
AnderCMD <andercmd@outlook.com>
2025-03-24 17:12:28 -0600

    Operador Condicional Ternario

---
+++ 03-Intro-JS/src/Bases/12-Operador-Condicional-Ternario.js
@@ -0,0 +1,8 @@
+// Declaración de una variable booleana para representar el estado
+const activo = true;
+
+// Uso de un operador lógico para asignar un mensaje basado en el estado (Esto es un operador ternario)
+const mensaje = activo && 'Activo';
+
+// Imprime el mensaje en la consola
+console.log(mensaje);
